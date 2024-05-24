4a3dbd55b8df031a0c77104461ae3a35ce1dfaad
Vishue1892 <victorsanchesbarrietos1203@gmail.com>
2024-05-24 12:56:46 -0600

    02 Poll App and Static

---
+++ poll/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from .models import Question, Choice
+# Register your models here.
+#admin.site.register(Question)
+#admin.site.register(Choice)
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    inlines=[ChoiceInLine]
---
+++ poll/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+app_name= 'poll'
+urlpatterns=[
+    #localhost:8000/poll/
+    path('',views.home,name='home'),
+    #localhost:8000/poll/1/vote/
+    path('<int:q_id>/vote/',views.vote, name='vote'),
+    #localhost:8000/poll/1/result/
+    path('<int:q_id>/result/',views.result,name='result')
+]
