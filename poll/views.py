b5abe1568be5a4a216e817630b345308e62d4697
Vishue1892 <victorsanchesbarrietos1203@gmail.com>
2024-06-03 12:31:53 -0600

    first commit

--- poll/views.py
+++ poll/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, redirect, render
 
-from .models import Question
+from .models import Question, Choice
 # Create your views here.
 def home(request):
     questions=Question.objects.all()
@@ -13,13 +13,19 @@ def home(request):
 def vote(request,q_id):
     q = get_object_or_404(Question, pk=q_id)
     if request.method == "POST":
-        choice_id = request.POST.get('choice')
-        choice = q.choice_set.get(pk=choice_id)
-        choice.votes += 1
-        choice.save()
-        return redirect('poll:result', q_id)
-    return render(request,'poll/vote.html',{
-        "question":q
+        try:
+            choice_id = request.POST.get('choice')
+            choice = q.choice_set.get(pk=choice_id)
+            choice.votes += 1
+            choice.save()
+            return redirect('poll:result', q_id)
+        except (KeyError, Choice.DoesNotExist):
+            return render(request,'poll/vote.html',{
+                "question": q,
+                "error_message":"Debes elegir algo! XD"
+            })
+    return render(request, 'poll/vote.html',{
+            "question":q
     })
 
 def result(request, q_id):
